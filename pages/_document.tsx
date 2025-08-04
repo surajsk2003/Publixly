9b3668982eea0f9ebfdeb2328b39705e59a7f613
Suraj kumar <surajkumarsksk2000@gmail.com>
2025-08-04 22:53:20 +0530

    The AdSense code has been successfully added to your _document.tsx file. Since this file is used by Next.js for all pages, the AdSense script   will now be included in the <head> section of every page across your site automatically.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo.png" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6171104448709105"
+             crossOrigin="anonymous"></script>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-6WL5XTWK4P"></script>
         <script
           dangerouslySetInnerHTML={{
